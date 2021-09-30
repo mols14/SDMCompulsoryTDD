b8216909848e8b5bf1490144d0a8c0eb1b6b11a5
Mathias Mols Thiim <math75n9@easv.dk>
2021-09-30 10:08:38 +0200

    blablaa

--- SDMCompulsoryTDD/Kernel/IReviewRepository.cs
+++ SDMCompulsoryTDD/Kernel/IReviewRepository.cs
@@ -6,16 +6,5 @@ namespace SDMCompulsoryTDD.Kernel
     public interface IReviewRepository
     {
         IEnumerable<BEReview> GetAll();
-
-        IEnumerable<int> GetReviewByReviewerNo(int reviewerNo);
-
-        IEnumerable<double> GetRatesByMovieId(int movieId);
-
-        
-        void Insert(BEReview r);
-
-        void Update(BEReview r);
-
-        void Delete(BEReview r);
     }
 }
